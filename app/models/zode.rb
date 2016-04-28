4914b1214b1af1674d2a231d0c7a16f7defe0021
Jethro Ordaneza <jet.oneza@gmail.com>
2016-04-28 21:36:15 +0800

    Zode model

---
+++ app/models/zode.rb
@@ -0,0 +1,11 @@
+# Zode Class
+# This just a fancy name for a place
+#
+# String name
+# Decimal latitude
+# Decimal longitude
+#
+# @author jet.oneza
+
+class Zode < ActiveRecord::Base
+end
---
+++ test/models/zode_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ZodeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
