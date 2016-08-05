4ab41cf646726053aeda507236d290eaa7d96040
Jethro Ordaneza <jet.oneza@gmail.com>
2016-08-05 19:50:19 +0800

    Implement re-rendering of map when receiving new route

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,6 +7,8 @@ class Map extends Component {
     this.map = null;
     this.google = null;
     this.mapInfo = null;
+    this.markers = [];
+    this.routePath = null;
   }
 
   componentDidMount() {
@@ -14,32 +16,28 @@ class Map extends Component {
   }
 
   componentDidUpdate() {
-    this.initMap();
+    this.loadMap();
   }
 
   initMap = () => {
-    const {mapCanvas} = this.refs;
     GoogleMap.KEY = 'AIzaSyDr8XyBDTZwhdBKTfAhoEZNN85NsLQxQxU';
     GoogleMap.LIBRARIES = ['geometry', 'places'];
     GoogleMap.SENSOR = false;
     GoogleMap.load((google) => {
       this.google = google;
       this.mapInfo = new google.maps.InfoWindow;
-      this.initialize(mapCanvas);
+      this.initialize();
+      this.loadMap();
     });
   }
 
-  initialize(mapCanvas) {
+  initialize() {
+    const {mapCanvas} = this.refs;
     const Google = this.google;
     const DAVAO = {lat: 7.057180, lng: 125.599512};
     const {activeRoute} = this.props;
     let center = DAVAO;
     let zoom = 13;
-
-    if (activeRoute && activeRoute.path) {
-      center = activeRoute.center;
-      zoom = activeRoute.zoom;
-    }
 
     this.map = new Google.maps.Map(mapCanvas, {
       center,
@@ -49,38 +47,79 @@ class Map extends Component {
         position: Google.maps.ControlPosition.TOP_RIGHT,
       },
     });
+  }
 
+  loadMap() {
+    const {activeRoute} = this.props;
     if (activeRoute && activeRoute.path) {
-      const {path} = activeRoute;
-      const routePath = new Google.maps.Polyline({
-        path,
-        geodesic: true,
-        strokeColor: '#2ecc71',
-        strokeOpacity: 0.7,
-        strokeWeight: 3
+      const center = activeRoute.center;
+      const zoom = activeRoute.zoom;
+      this.map.setOptions({center, zoom});
+      this.setPath(activeRoute);
+    } else {
+      this.initialize()
+    }
+  }
+
+
+  setPath(activeRoute) {
+    const {path} = activeRoute;
+    const Google = this.google;
+    this.clearMap();
+    this.routePath = new Google.maps.Polyline({
+      path,
+      geodesic: true,
+      strokeColor: '#2ecc71',
+      strokeOpacity: 0.7,
+      strokeWeight: 3
+    });
+
+    let index = 0;
+    activeRoute.markers.forEach((marker) => {
+      const {lat, lng} = marker;
+
+      let options = {
+        position: {lat, lng},
+        icon: {
+          path: Google.maps.SymbolPath.CIRCLE,
+          scale: 5,
+          strokeColor: '#e74c3c',
+          strokeWeight: 4,
+          fillColor: '#fff',
+          fillOpacity: 0.7
+        },
+        map: this.map,
+      };
+
+      if (index === 0) {
+        // TODO: Change starting icon image
+        const image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        options.animation = Google.maps.Animation.DROP;
+        options.icon = image;
+      }
+
+      const mapMarker = new Google.maps.Marker(options);
+
+      mapMarker.addListener('click', (e) => {
+        this.displayMarker(e, marker);
       });
 
-      activeRoute.markers.forEach((marker) => {
-        const {lat, lng} = marker;
-        const mapMarker = new Google.maps.Marker({
-          position: {lat, lng},
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 5,
-            strokeColor: '#e74c3c',
-            strokeWeight: 4,
-            fillColor: '#fff',
-            fillOpacity: 0.7
-          },
-          map: this.map,
-        });
+      this.markers.push(mapMarker);
+      index++;
+    });
 
-        mapMarker.addListener('click', (e) => {
-          this.displayMarker(e, marker);
-        });
-      });
+    this.routePath.setMap(this.map);
+  }
 
-      routePath.setMap(this.map);
+  clearMap() {
+    this.markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    this.markers = [];
+
+    if (this.routePath) {
+      this.routePath.setMap(null);
+      this.routePath = null;
     }
   }
 
